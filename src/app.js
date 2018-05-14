6f2751fa26394ce45488d8d15ba58862f706e927
kyo5884 <kyo5884.public@gmail.com>
2018-05-14 14:18:53 +0900

    added Tab

--- src/app.js
+++ src/app.js
@@ -3,22 +3,72 @@ const Header = require('./header.js');
 const colors = require('./colors.js');
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTab: 'Home'
+        }
+    }
+
+    selectTab(name) {
+        this.setState({selectedTab: name});
+    }
+
     render() {
         return (
             <div>
-                <Header />
-                <div style={{
-                    position: 'absolute',
-                    top: 40,
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    overflow: scroll,
-                    backgroundColor: colors.background
-                }}>
-
-                </div>
+                <Header
+                    selectTab={(tabName) => this.selectTab(tabName)} 
+                    selectedTab={this.state.selectedTab}
+                />
+                <Tab>
+                    <TabPanel selected={this.state.selectedTab == 'Home'}>
+                        Home
+                    </TabPanel>
+                    <TabPanel selected={this.state.selectedTab == 'Mentions'}>
+                        Mentions
+                    </TabPanel>
+                    <TabPanel selected={this.state.selectedTab == 'Interactions'}>
+                        Interactions
+                    </TabPanel>
+                    <TabPanel selected={this.state.selectedTab == 'Global'}>
+                        Global
+                    </TabPanel>
+                    <TabPanel selected={this.state.selectedTab == 'User'}>
+                        User
+                    </TabPanel>
+                </Tab>
             </div>
+        );
+    }
+}
+
+class Tab extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div style={{
+                position: 'absolute',
+                top: 40,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                overflow: scroll,
+                backgroundColor: colors.background
+            }}>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+class TabPanel extends React.Component {
+    render() {
+        return(
+            <div>{this.props.selected ? this.props.children : null}</div>
         );
     }
 }
--- src/header.js
+++ src/header.js
@@ -3,6 +3,10 @@ const colors = require('./colors.js');
 
 class Header extends React.Component {
 
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div style={{
@@ -15,30 +19,48 @@ class Header extends React.Component {
                 WebkitAppRegion: 'drag'
             }}>
                 <div style={{width: '56px', height: '100%'}} />
-                <div style={{
-                    
-                }}>
+                <div>
                     <HeaderButton
                         icon_name='home'
-                        active
+                        tab_name='Home'
+                        selectTab={this.props.selectTab}
+                        selectedTab={this.props.selectedTab}
                     />
                     <HeaderButton
                         icon_name='at'
+                        tab_name='Mentions'
+                        selectTab={this.props.selectTab}
+                        selectedTab={this.props.selectedTab}
                     />
                     <HeaderButton
                         icon_name='bullseye'
+                        tab_name='Interactions'
+                        selectTab={this.props.selectTab}
+                        selectedTab={this.props.selectedTab}
                     />
                     <HeaderButton
                         icon_name='globe'
+                        tab_name='Global'
+                        selectTab={this.props.selectTab}
+                        selectedTab={this.props.selectedTab}
                     />
                     <HeaderButton
                         icon_name='user'
+                        tab_name='User'
+                        selectTab={this.props.selectTab}
+                        selectedTab={this.props.selectedTab}
                     />
                 </div>
-                <div style={{marginRight: '5px'}}>
-                <HeaderButton
-                    icon_name='pencil'
-                />
+                <div style={{marginRight: '5px'}} onClick={() => console.log('New Post')}>
+                    <FontAwesome
+                        name={'pencil'} 
+                        style={{
+                            fontSize: '18px',
+                            margin: 0,
+                            padding: '5px',
+                            color: colors.button.inactive
+                        }}
+                    />
                 </div>
             </div>
         );
@@ -53,15 +75,17 @@ class HeaderButton extends React.Component {
 
     render() {
         return (
-            <FontAwesome
-                name={this.props.icon_name} 
-                style={{
-                    fontSize: '18px',
-                    margin: 0,
-                    padding: '5px',
-                    color: this.props.active ? colors.button.active : colors.button.inactive
-                }}
-            />
+            <a onClick={() => this.props.selectTab(this.props.tab_name)}>
+                <FontAwesome
+                    name={this.props.icon_name} 
+                    style={{
+                        fontSize: '18px',
+                        margin: 0,
+                        padding: '5px',
+                        color: this.props.selectedTab == this.props.tab_name ? colors.button.active : colors.button.inactive
+                    }}
+                />
+            </a>
         );
     }
 }
